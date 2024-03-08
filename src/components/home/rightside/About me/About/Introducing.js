7bc841b8379e2141e719a8460c1a0e6a3fc1c313
Abdullah Arafa <abdullahciti@gmx.de>
2024-03-08 10:52:16 +0100

    Bio modified

--- src/components/home/rightside/About me/About/Introducing.js
+++ src/components/home/rightside/About me/About/Introducing.js
@@ -3,7 +3,7 @@ const Introducing = () => {
     <div className="introducing p-3">
       <h2 className="text-xl font-semibold text-zinc-300 px-4">Hello I`m Abdullah Arafa</h2>
       <p className="text-zinc-400 max-w-[80%] p-4">
-        Web designer from Egypt, Giza. I`ve rich experiance in website
+        Web developer from Egypt, Giza. I`ve rich experiance in website
         development, also i`m good at JavaScript. I`d like to talk with you about
         my new position!
       </p>
